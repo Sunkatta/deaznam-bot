542308c8683337efce29f586a56dae4dc80bd98b
Lusian Manolov <lusianmanolov@outlook.com>
2021-09-25 12:22:36 +0300

    Initial bot setup

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if (message.author == client.user):
+        return
+
+    if (message.content.startswith('/hello')):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('BOT_AUTH_TOKEN'))
