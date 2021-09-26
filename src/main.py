8e8263c44a7e48be0c377fe66356991d103229d8
Lusian Manolov <lusianmanolov@outlook.com>
2021-09-26 15:57:04 +0300

    Added basic music stream integration

--- src/main.py
+++ src/main.py
@@ -1,22 +1,126 @@
 import os
-from dotenv import load_dotenv
+import asyncio
 import discord
+import youtube_dl
+
+from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
-client = discord.Client()
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
 
 
-@client.event
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+# TODO: Extract in different file/module/package
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("/"),
+                   description="I don't know where this text is gonna be visualised...")
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
 
-@client.event
-async def on_message(message):
-    if (message.author == client.user):
-        return
+@bot.command()
+async def repeat(ctx, *, arg):
+    await ctx.send(arg)
 
-    if (message.content.startswith('/hello')):
-        await message.channel.send('Hello!')
 
-client.run(os.getenv('BOT_AUTH_TOKEN'))
+@repeat.error
+async def repeat_error(ctx, error):
+    await ctx.send('My creators are dumbasses and did not teach me how to repeat this...')
+
+
+# TODO: Extract in different file/module/package
+class Music(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def ddzjoin(self, ctx, *, channel: discord.VoiceChannel):
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player,
+                                  after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(player.title))
+
+    @commands.command()
+    async def ddzvolume(self, ctx, volume: int):
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send("Changed volume to {}%".format(volume))
+
+    @commands.command()
+    async def ddzstop(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send('You are not connected to a voice channel.')
+                raise commands.CommandError(
+                    'Author not connected to a voice channel.')
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+
+bot.add_cog(Music(bot))
+bot.run(os.getenv('BOT_AUTH_TOKEN'))
