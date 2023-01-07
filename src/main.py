0c25e1375de3c9c72ae2a94f2cbf5b1877d8269c
Lusian Manolov <lusianmanolov@outlook.com>
2023-01-07 15:22:49 +0200

    Upgraded code to use latest dependecy versions

--- src/main.py
+++ src/main.py
@@ -4,14 +4,18 @@ import os
 import discord
 import asyncio
 from discord.ext import commands
-from discord_slash import SlashCommand
+from discord.ext.commands import Greedy, Context
 from music import Music
 from dotenv import load_dotenv
+from typing import Literal, Optional
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("/"))
-slash = SlashCommand(bot, sync_commands=True)
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("/"),
+                   intents=intents)
 
 
 @bot.event
@@ -41,18 +45,52 @@ async def on_voice_state_update(member, before, after):
                 break
 
 
-@slash.slash(
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+
+@bot.tree.command(
     name='say',
     description='Repeat a phrase'
 )
-async def say(ctx, *, input):
-    await ctx.send(input)
+async def say(interaction: discord.Interaction, *, input: str):
+    await interaction.response.send_message(input)
 
 
 @say.error
-async def say_error(ctx, error):
-    await ctx.send('My creators are dumbasses and did not teach me how to repeat this...')
+async def say_error(interaction: discord.Interaction, error):
+    await interaction.response.send_message('My creators are dumbasses and did not teach me how to repeat this...')
 
 
-bot.add_cog(Music(bot))
+asyncio.run(bot.add_cog(Music(bot)))
 bot.run(os.getenv('BOT_AUTH_TOKEN'))
--- src/music.py
+++ src/music.py
@@ -2,10 +2,8 @@ import discord
 import asyncio
 import youtube_dl
 from discord.voice_client import VoiceClient
-from discord_slash.model import SlashCommandOptionType
-from discord_slash.utils.manage_commands import create_option
 from discord.ext import commands
-from discord_slash import cog_ext, SlashContext
+from discord import app_commands
 from queue import Queue
 from song import Song
 
@@ -37,53 +35,38 @@ class Music(commands.Cog):
         self.currentSong: Song = None
         self.songQueue = Queue()
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='join',
         description='Joins the specified channel'
     )
-    async def join(self, ctx: SlashContext, channel: discord.VoiceChannel):
+    async def join(self, interaction: discord.Interaction, channel: discord.VoiceChannel):
         try:
-            await ctx.defer()
-            voice_client: VoiceClient = ctx.voice_client
+            voice_client: VoiceClient = interaction.guild.voice_client
 
             if voice_client is not None:
                 return await voice_client.move_to(channel)
 
             await channel.connect()
-            await ctx.send(f'Joined channel: `{channel.name}`')
+            await interaction.response.send_message(f'Joined channel: `{channel.name}`')
         except:
-            await ctx.send('I did an whoopsie... Please try that again...')
+            await interaction.response.send_message('I did an whoopsie... Please try that again...')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='play',
         description='Time to get funky',
-        options=[
-            create_option(
-                name='input',
-                description='URL or search term',
-                required=True,
-                option_type=SlashCommandOptionType.STRING
-            ),
-            create_option(
-                name='channel',
-                description='Target channel',
-                required=False,
-                option_type=SlashCommandOptionType.CHANNEL
-            )
-        ]
     )
-    async def play(self, ctx: SlashContext, input: str, channel: discord.VoiceChannel = None):
+    async def play(self, interaction: discord.Interaction, input: str, channel: discord.VoiceChannel = None):
         try:
-            await ctx.defer()
+            await interaction.response.defer()
 
-            if ctx.voice_client is None:
-                if ctx.author.voice is not None:
-                    authorChannel = ctx.author.voice.channel
+            if interaction.guild.voice_client is None:
+                if interaction.user.voice is not None:
+                    authorChannel = interaction.user.voice.channel
                     await authorChannel.connect()
                 elif channel is not None:
                     await channel.connect()
                 else:
-                    await ctx.send('Join a voice channel, dummy')
+                    await interaction.response.send_message('Join a voice channel, dummy')
                     return
 
             loop = self.bot.loop or asyncio.get_event_loop()
@@ -92,115 +75,118 @@ class Music(commands.Cog):
 
             if 'entries' in data:
                 for entry in data['entries']:
-                    song = Song(entry['title'], entry['webpage_url'],
+                    song = Song(entry['title'],
+                                entry['webpage_url'],
                                 discord.FFmpegPCMAudio(entry['url'], **ffmpeg_options))
+
                     songsToEnqueue.append(song)
             else:
-                song = Song(data['title'], data['webpage_url'], discord.FFmpegPCMAudio(
-                    data['url'], **ffmpeg_options))
+                song = Song(data['title'],
+                            data['webpage_url'],
+                            discord.FFmpegPCMAudio(data['url'], **ffmpeg_options))
+
                 songsToEnqueue.append(song)
 
             list(map(self.songQueue.put, songsToEnqueue))
 
-            if not ctx.voice_client.is_playing():
-                self.play_song(ctx)
+            if not interaction.guild.voice_client.is_playing():
+                self.play_song(interaction)
 
                 if self.songQueue.qsize() == 0:
-                    await ctx.send(f'Now playing: `{self.currentSong.title} - {self.currentSong.webpage_url}`')
+                    await interaction.followup.send(f'Now playing: `{self.currentSong.title} - {self.currentSong.webpage_url}`')
                 else:
-                    await ctx.send(f'Queued {len(songsToEnqueue)} songs')
+                    await interaction.followup.send(f'Queued {str(len(songsToEnqueue))} songs')
             elif self.songQueue.qsize() == 1 and len(songsToEnqueue) == 1:
-                await ctx.send(f'Next up: `{songsToEnqueue[0].title} - {songsToEnqueue[0].webpage_url}`')
+                await interaction.followup.send(f'Next up: `{songsToEnqueue[0].title} - {songsToEnqueue[0].webpage_url}`')
             else:
-                await ctx.send(f'Queued {len(songsToEnqueue)} songs')
+                await interaction.followup.send(f'Queued {str(len(songsToEnqueue))} songs')
         except:
-            await ctx.send('I did an whoopsie... Please try that again...')
+            await interaction.followup.send('I did an whoopsie... Please try that again...')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='volume',
         description='Tweak the master volume when in voice channel'
     )
-    async def volume(self, ctx: SlashContext, volume: int):
-        voice_client: VoiceClient = ctx.voice_client
+    async def volume(self, interaction: discord.Interaction, volume: int):
+        voice_client: VoiceClient = interaction.guild.voice_client
 
         if voice_client is None:
-            return await ctx.send('Not connected to a voice channel')
+            return await interaction.response.send_message('Not connected to a voice channel')
 
         voice_client.source.volume = volume / 100
-        await ctx.send(f'Changed volume to {volume}%')
+        await interaction.response.send_message(f'Changed volume to {volume}%')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='stop',
         description='Stop the funk'
     )
-    async def stop(self, ctx: SlashContext):
-        voice_client: VoiceClient = ctx.voice_client
+    async def stop(self, interaction: discord.Interaction):
+        voice_client: VoiceClient = interaction.guild.voice_client
         voice_client.stop()
         self.songQueue.queue.clear()
-        await ctx.send('Stopped')
+        await interaction.response.send_message('Stopped')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='skip',
         description='Skip the current song'
     )
-    async def skip(self, ctx: SlashContext):
-        await ctx.defer()
-
+    async def skip(self, interaction: discord.Interaction):
         if self.songQueue.empty():
-            await ctx.send("Can't skip the void, bozo")
+            await interaction.response.send_message("Can't skip the void, bozo")
         else:
-            voice_client: VoiceClient = ctx.voice_client
+            voice_client: VoiceClient = interaction.guild.voice_client
             voice_client.stop()
-            await ctx.send('Skipped')
+            await interaction.response.send_message('Skipped')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='resume',
         description='Resume the funk'
     )
-    async def resume(self, ctx: SlashContext):
-        voice_client: VoiceClient = ctx.voice_client
+    async def resume(self, interaction: discord.Interaction):
+        voice_client: VoiceClient = interaction.guild.voice_client
         voice_client.resume()
-        await ctx.send('Resumed')
+        await interaction.response.send_message('Resumed')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='pause',
         description='Pause the funk'
     )
-    async def pause(self, ctx: SlashContext):
-        voice_client: VoiceClient = ctx.voice_client
+    async def pause(self, interaction: discord.Interaction):
+        voice_client: VoiceClient = interaction.guild.voice_client
         voice_client.pause()
-        await ctx.send('Paused')
+        await interaction.response.send_message('Paused')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='disconnect',
         description='Disconnect from the current voice channel'
     )
-    async def disconnect(self, ctx: SlashContext):
-        voice_client: VoiceClient = ctx.voice_client
+    async def disconnect(self, interaction: discord.Interaction):
+        voice_client: VoiceClient = interaction.guild.voice_client
+        voice_client.stop()
+        self.songQueue.queue.clear()
         await voice_client.disconnect()
-        await ctx.send('Sayonara, losers')
+        await interaction.response.send_message('Sayonara, losers')
 
-    @cog_ext.cog_slash(
+    @app_commands.command(
         name='queue',
         description='Display all upcoming songs'
     )
-    async def queue(self, ctx: SlashContext):
-        await ctx.defer()
+    async def queue(self, interaction: discord.Interaction):
         message = '`'
         i = 1
 
         if self.songQueue.empty():
-            return await ctx.send('Queue is empty')
+            return await interaction.response.send_message('Queue is empty')
         else:
             for item in self.songQueue.queue:
                 message += f'{i}.{item.title}\n'
                 i += 1
 
             message += '`'
-            return await ctx.send(message)
+            return await interaction.response.send_message(message)
 
-    def play_song(self, ctx: SlashContext):
+    def play_song(self, interaction: discord.Interaction):
         if not self.songQueue.empty():
             self.currentSong: Song = self.songQueue.get()
-            ctx.voice_client.play(self.currentSong.player,
-                                  after=lambda e: self.play_song(ctx))
+            interaction.guild.voice_client.play(self.currentSong.player,
+                                                after=lambda e: self.play_song(interaction))
