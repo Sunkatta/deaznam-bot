f17b33052e9a145db75178eea7ddabad456133a2
Lusian Manolov <lusianmanolov@outlook.com>
2025-07-05 18:25:46 +0300

    Added support for more than one environment (#9)

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
 import os
+import argparse
+import asyncio
 from discord.ext import commands
 from setup import setup
 from dotenv import load_dotenv
 
-load_dotenv()
 
-bot: commands.Bot = setup()
-bot.run(os.getenv('BOT_AUTH_TOKEN'))
+async def main(env: str):
+
+    # If no argument is specified, use default env for local development.
+    dotenv_file = f".env.{env}" if env is not None else ".env"
+    print(f'Using environment file {dotenv_file}')
+    load_dotenv(dotenv_file, override=True)
+
+    token = os.getenv('BOT_AUTH_TOKEN')
+
+    bot: commands.Bot = await setup()
+    await bot.start(token)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env')
+    args = parser.parse_args()
+
+    asyncio.run(main(args.env))
--- src/setup.py
+++ src/setup.py
@@ -1,18 +1,13 @@
 import discord
-import asyncio
-from cogs.music.music import Music
-from cogs.general.general import General
+from deaznam_bot import DeaznamBot
 from discord.ext import commands
 
 
-def setup() -> commands.Bot:
+async def setup() -> commands.Bot:
     intents = discord.Intents.default()
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix=commands.when_mentioned_or("/"),
-                       intents=intents)
-
-    asyncio.run(bot.add_cog(General(bot)))
-    asyncio.run(bot.add_cog(Music(bot)))
+    bot = DeaznamBot(command_prefix=commands.when_mentioned_or("/"),
+                     intents=intents)
 
     return bot
