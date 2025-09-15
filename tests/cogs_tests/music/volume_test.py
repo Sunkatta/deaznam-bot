e9bfb08ec4061adec0a97dddb46d6626baf7663c
Lusian Manolov <lusianmanolov@outlook.com>
2025-09-15 20:20:02 +0300

    Fixed volume tests

--- tests/cogs_tests/music/volume_test.py
+++ tests/cogs_tests/music/volume_test.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import AsyncMock, MagicMock
 
 from cogs.music.music import Music
+from services.music_service import MusicService
 
 
 class VolumeTest(unittest.IsolatedAsyncioTestCase):
@@ -16,7 +17,9 @@ class VolumeTest(unittest.IsolatedAsyncioTestCase):
         mock_bot = MagicMock()
         mock_bot.loop = None
 
-        music_cog = Music(mock_bot)
+        mock_music_service = MusicService()
+
+        music_cog = Music(mock_bot, mock_music_service)
 
         # Act
         await music_cog.volume.callback(music_cog, mock_interaction, volume)
@@ -40,7 +43,9 @@ class VolumeTest(unittest.IsolatedAsyncioTestCase):
         mock_bot = MagicMock()
         mock_bot.loop = None
 
-        music_cog = Music(mock_bot)
+        mock_music_service = MusicService()
+
+        music_cog = Music(mock_bot, mock_music_service)
 
         # Act
         await music_cog.volume.callback(music_cog, mock_interaction, volume)
