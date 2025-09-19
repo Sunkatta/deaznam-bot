cef5576f30e368c2de40f522e8fa41af5ca03136
Lusian Manolov <lusianmanolov@outlook.com>
2025-09-19 19:23:52 +0300

    Fixed skip tests

--- tests/cogs_tests/music/skip_test.py
+++ tests/cogs_tests/music/skip_test.py
@@ -2,17 +2,12 @@ import unittest
 from unittest.mock import AsyncMock, MagicMock
 
 from cogs.music.music import Music
-from models.song import Song
-from services.music_service import MusicService
 
 
 class SkipTest(unittest.IsolatedAsyncioTestCase):
     async def test_should_successfully_skip_the_current_song(self):
         # Arrange
         player_id = "1"
-        song_title = "Never Gonna Give You Up"
-        song_url = "www.rickroll.com"
-
         mock_interaction = MagicMock()
         mock_interaction.response.send_message = AsyncMock()
         mock_interaction.guild.voice_client = MagicMock()
@@ -21,18 +16,19 @@ class SkipTest(unittest.IsolatedAsyncioTestCase):
         mock_bot = MagicMock()
         mock_bot.loop = None
 
-        mock_music_service = MusicService()
+        mock_music_queue = MagicMock()
+        mock_music_queue.empty.return_value = False
+
+        mock_music_service = MagicMock()
+        mock_music_service.get_song_queue_by_music_player_id = MagicMock()
+        mock_music_service.get_song_queue_by_music_player_id.return_value = mock_music_queue
 
         music_cog = Music(mock_bot, mock_music_service)
-        mock_music_service.add_music_player(player_id)
-        music_player = mock_music_service.music_players[player_id]
-        music_player.add_song(Song(song_title, song_url, None))
 
         # Act
         await music_cog.skip.callback(music_cog, mock_interaction)
 
         # Assert
-        self.assertEqual(music_player.song_queue.qsize(), 0)
         mock_interaction.guild.voice_client.stop.assert_called_once()
         mock_interaction.response.send_message.assert_called_once_with(
             'Skipped')
@@ -46,7 +42,12 @@ class SkipTest(unittest.IsolatedAsyncioTestCase):
         mock_bot = MagicMock()
         mock_bot.loop = None
 
-        mock_music_service = MusicService()
+        mock_music_queue = MagicMock()
+        mock_music_queue.empty.return_value = True
+
+        mock_music_service = MagicMock()
+        mock_music_service.get_song_queue_by_music_player_id = MagicMock()
+        mock_music_service.get_song_queue_by_music_player_id.return_value = mock_music_queue
 
         music_cog = Music(mock_bot, mock_music_service)
 
@@ -54,7 +55,6 @@ class SkipTest(unittest.IsolatedAsyncioTestCase):
         await music_cog.skip.callback(music_cog, mock_interaction)
 
         # Assert
-        self.assertEqual(len(mock_music_service.music_players), 0)
         self.assertEqual(
             mock_interaction.guild.voice_client.stop.call_count, 0)
         mock_interaction.response.send_message.assert_called_once_with(
